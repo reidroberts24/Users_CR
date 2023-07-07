345b12c29bbdca6534815aa581ec6b2b126b49f5
Reid Roberts <reidrobertscs@gmail.com>
2023-07-06 21:13:37 -0500

    added CRUD functionality

--- user.py
+++ user.py
@@ -19,9 +19,37 @@ class User:
         return users
     
     @classmethod
+    def get_user(cls, id):
+        query = 'SELECT * FROM users WHERE id=%(id)s'
+        results = connectToMySQL('users').query_db(query, {'id': id})
+        return cls(results[0])
+
+    @classmethod
     def save(cls, data):
         query = '''INSERT INTO users (first_name, last_name, email, created_at, updated_at) 
         VALUES (%(first)s, %(last)s, %(email)s, NOW(), NOW())
         '''
         return connectToMySQL('users').query_db(query, data)
+    
+    @classmethod
+    def update(cls, data):
+        query = """
+            UPDATE users
+            SET first_name = %(first)s, 
+                last_name = %(last)s, 
+                email = %(email)s, 
+                updated_at = NOW()
+                WHERE id = %(id)s;
+            """
+        results = connectToMySQL('users').query_db(query, data)
+        return results
+    
+    @classmethod
+    def delete(cls, id):
+        query = """
+            DELETE FROM users
+            WHERE id = %(id)s;
+        """
+        results = connectToMySQL('users').query_db(query, {"id": id})
+        return results
     
